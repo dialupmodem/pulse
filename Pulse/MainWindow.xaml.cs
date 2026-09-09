f103fd19a68d426e375668178471b6143be9cd98
dialupmodem <18317934+dialupmodem@users.noreply.github.com>
2026-09-09 05:34:27 -0400

    add WPF-UI

--- Pulse/MainWindow.xaml.cs
+++ Pulse/MainWindow.xaml.cs
@@ -10,13 +10,14 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using Wpf.Ui.Controls;
 
 namespace Pulse
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class MainWindow : FluentWindow
     {
         public MainWindow()
         {
